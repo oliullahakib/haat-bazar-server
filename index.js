14fb5f09de15afa690de863d2f31811a4b3cbf18
oliullahakib <oliullahakib@gmail.com>
2025-11-24 09:42:13 +0600

    initialize the server and added api for getting all products

---
+++ index.js
@@ -0,0 +1,63 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express')
+require('dotenv').config()
+const cors = require('cors');
+const app = express()
+const port = process.env.PORT || 3100
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wfr9cox.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+// middleware 
+app.use(cors())
+app.use(express.json())
+
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const db = client.db("haat-bazar")
+    const productsCollection = db.collection("product")
+
+    app.get('/products',async(req,res)=>{
+        const {short:shortValu,limit:limitValue}=req.query
+        const short = {}
+        let limit = 0
+        if(shortValu==="true"){
+            short.rating= -1
+        }
+        if(limitValue){
+          limit = parseInt(limitValue)
+        }
+        console.log(limit)
+        const result = await productsCollection.find().sort(short).limit(limit).toArray()
+        res.send(result)
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
